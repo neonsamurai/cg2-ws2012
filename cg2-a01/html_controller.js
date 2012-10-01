441f61c7baec4f2f3beab2fea1202ebae277982b
Tim Jagodzinski <tim.jagodzinski@mtc-berlin.com>
2012-10-01 15:59:23 +0200

    adds exercises 1.1.1-2. adds stubs for 1.1.3 (color picker)

--- cg2-a01/html_controller.js
+++ cg2-a01/html_controller.js
@@ -1,87 +1,98 @@
 /*
- * JavaScript / Canvas teaching framwork 
- * (C)opyright Hartmut Schirmacher, hschirmacher.beuth-hochschule.de 
+ * JavaScript / Canvas teaching framwork
+ * (C)opyright Hartmut Schirmacher, hschirmacher.beuth-hochschule.de
  *
  * Module: html_controller
  *
- * Defines callback functions for communicating with various 
+ * Defines callback functions for communicating with various
  * HTML elements on the page, e.g. buttons and parameter fields.
  *
  */
 
- 
 /* requireJS module definition */
-define(["jquery", "straight_line"], 
-       (function($, StraightLine) {
+define(["jquery", "straight_line", "circle"], (function($, StraightLine, Circle) {"use strict";
 
-    "use strict"; 
-                
-    /*
-     * define callback functions to react to changes in the HTML page
-     * and provide them with a closure defining context and scene
-     */
-    var HtmlController = function(context,scene,sceneController) {
-    
-    
-        // generate random X coordinate within the canvas
-        var randomX = function() { 
-            return Math.floor(Math.random()*(context.canvas.width-10))+5; 
-        };
-            
-        // generate random Y coordinate within the canvas
-        var randomY = function() { 
-            return Math.floor(Math.random()*(context.canvas.height-10))+5; 
-        };
-            
-        // generate random color in hex notation
-        var randomColor = function() {
-
-            // convert a byte (0...255) to a 2-digit hex string
-            var toHex2 = function(byte) {
-                var s = byte.toString(16); // convert to hex string
-                if(s.length == 1) s = "0"+s; // pad with leading 0
-                return s;
-            };
-                
-            var r = Math.floor(Math.random()*25.9)*10;
-            var g = Math.floor(Math.random()*25.9)*10;
-            var b = Math.floor(Math.random()*25.9)*10;
-                
-            // convert to hex notation
-            return "#"+toHex2(r)+toHex2(g)+toHex2(b);
-        };
-        
         /*
-         * event handler for "new line button".
+         * define callback functions to react to changes in the HTML page
+         * and provide them with a closure defining context and scene
          */
-        $("#btnNewLine").click( (function() {
-        
-            // create the actual line and add it to the scene
-            var style = { 
-                width: Math.floor(Math.random()*3)+1,
-                color: randomColor()
-            };
-                          
-            var line = new StraightLine( [randomX(),randomY()], 
-                                         [randomX(),randomY()], 
-                                         style );
-            scene.addObjects([line]);
+        var HtmlController = function(context, scene, sceneController) {
 
-            // deselect all objects, then select the newly created object
-            sceneController.deselect();
-            sceneController.select(line); // this will also redraw
-                        
-        }));
-        
-    
-    };
+                // generate random X coordinate within the canvas
+                var randomX = function() {
+                        return Math.floor(Math.random() * (context.canvas.width - 10)) + 5;
+                };
 
-    // return the constructor function 
-    return HtmlController;
+                // generate random Y coordinate within the canvas
+                var randomY = function() {
+                        return Math.floor(Math.random() * (context.canvas.height - 10)) + 5;
+                };
 
+                // generate random color in hex notation
+                var randomColor = function() {
 
-})); // require 
+                        // convert a byte (0...255) to a 2-digit hex string
+                        var toHex2 = function(byte) {
+                                var s = byte.toString(16);
+                                // convert to hex string
+                                if (s.length == 1)
+                                        s = "0" + s;
+                                // pad with leading 0
+                                return s;
+                        };
 
+                        var r = Math.floor(Math.random() * 25.9) * 10;
+                        var g = Math.floor(Math.random() * 25.9) * 10;
+                        var b = Math.floor(Math.random() * 25.9) * 10;
 
+                        // convert to hex notation
+                        return "#" + toHex2(r) + toHex2(g) + toHex2(b);
+                };
 
-            
+                /*
+                 * event handler for "new line button".
+                 */
+                $("#btnNewLine").click((function() {
+
+                        // create the actual line and add it to the scene
+                        var style = {
+                                width : Math.floor(Math.random() * 3) + 1,
+                                color : randomColor()
+                        };
+
+                        var line = new StraightLine([randomX(), randomY()], [randomX(), randomY()], style);
+                        scene.addObjects([line]);
+
+                        // deselect all objects, then select the newly created object
+                        sceneController.deselect();
+                        sceneController.select(line);
+                        // this will also redraw
+
+                }));
+
+                $("#btnNewCircle").click((function() {
+                        var style = {
+                                width : Math.floor(Math.random() * 3) + 1,
+                                color : randomColor()
+                        };
+
+                        var circle = new Circle([randomX(), randomY()], randomX()/4 + 15, style)
+                        scene.addObjects([circle]);
+
+                        sceneController.deselect();
+                        sceneController.select(circle);
+                }));
+                $("#colorPicker").change((function(){
+                      console.log($("#colorPicker").val()) ;
+                      var obj = sceneController.getSelectedObject();
+                      obj.lineStyle.color = $("#colorPicker").val();
+                }));
+
+        };
+
+        // return the constructor function
+        return HtmlController;
+
+}));
+// require
+
--- cg2-a01/vec2.js
+++ cg2-a01/vec2.js
@@ -1,6 +1,6 @@
 /*
- * JavaScript / Canvas teaching framwork 
- * (C)opyright Hartmut Schirmacher, hschirmacher.beuth-hochschule.de 
+ * JavaScript / Canvas teaching framwork
+ * (C)opyright Hartmut Schirmacher, hschirmacher.beuth-hochschule.de
  *
  * Module: vec2
  *
@@ -8,59 +8,63 @@
  *
  */
 
-
 /* requireJS module definition */
-define([], 
-       (function() {
-       
-    "use strict";
-    
-    var vec2 = {};
+define([], (function() {"use strict";
 
-    // add two vectors, return new vector 
-    vec2.add = function(v0,v1) {
-        return [v0[0] + v1[0], v0[1] + v1[1] ];
-    };
+        var vec2 = {};
 
-    // subtract two vectors, return new vector 
-    vec2.sub = function(v0,v1) {
-        return [v0[0] - v1[0], v0[1] - v1[1] ];
-    };
+        // add two vectors, return new vector
+        vec2.add = function(v0, v1) {
+                return [v0[0] + v1[0], v0[1] + v1[1]];
+        };
 
-    // scalar product of two vectors, return scalar 
-    vec2.dot = function(v0,v1) {
-        return v0[0] * v1[0] + v0[1] * v1[1];
-    };
-    
-    // multiply vector by scalar, return new vector
-    vec2.mult = function(v,s) {
-        return [ v[0]*s, v[1]*s ];
-    };
-    
-    // squared length of a vector
-    vec2.length2 = function(v) {
-        return vec2.dot(v,v);
-    };
-    
-    // length of a vector
-    vec2.length = function(v) {
-        return Math.sqrt(vec2.length2(v));
-    };
-    
-    // project a point onto a line defined by two points.
-    // return scalar parameter of where point p is projected on line
-    vec2.projectPointOnLine = function(p, p0,p1) {
+        // subtract two vectors, return new vector
+        vec2.sub = function(v0, v1) {
+                return [v0[0] - v1[0], v0[1] - v1[1]];
+        };
 
-        var dp = vec2.sub(p,p0);
-        var dv = vec2.sub(p1,p0);
-        var t  = vec2.dot(dp,dv)/vec2.dot(dv,dv);
-        return t;
-                
-    };
-    
-    // this module exports an object defining a number of functions
-    return vec2;
+        // scalar product of two vectors, return scalar
+        vec2.dot = function(v0, v1) {
+                return v0[0] * v1[0] + v0[1] * v1[1];
+        };
 
-})); // define
+        // multiply vector by scalar, return new vector
+        vec2.mult = function(v, s) {
+                return [v[0] * s, v[1] * s];
+        };
 
-    
+        // squared length of a vector
+        vec2.length2 = function(v) {
+                return vec2.dot(v, v);
+        };
+
+        // length of a vector
+        vec2.length = function(v) {
+                return Math.sqrt(vec2.length2(v));
+        };
+
+        // project a point onto a line defined by two points.
+        // return scalar parameter of where point p is projected on line
+        vec2.projectPointOnLine = function(p, p0, p1) {
+
+                var dp = vec2.sub(p, p0);
+                var dv = vec2.sub(p1, p0);
+                var t = vec2.dot(dp, dv) / vec2.dot(dv, dv);
+                return t;
+
+        };
+
+        //project a point onto a circle line defined by center point and radius
+        // returns 0 when p is on the circle outline
+        vec2.projectPointOnCircle = function(p, cp, r) {
+                var distanceVector = vec2.sub(p, cp);
+                return vec2.length(distanceVector) - r;
+
+        };
+
+        // this module exports an object defining a number of functions
+        return vec2;
+
+}));
+// define
+
